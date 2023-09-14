69c6abdda8d21c4d33f8e99543153a0e209afeea
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-09-14 23:11:52 +0200

    Offer component finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Offers from "./components/Offers";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }
